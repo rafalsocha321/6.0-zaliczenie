b0fc69cf10ee0da1b20be004983dec3476c07adb
rafalsocha321 <126696041+rafalsocha321@users.noreply.github.com>
2023-05-24 14:35:24 +0000

    rozwiązanie

--- Main.java
+++ Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Wprowadź 10 liczb całkowitych"); 
+    int tab[] = new int[10];
+
+    Scanner scanner = new Scanner(System.in);
+     for(int i=0; i<10; i++)
+       {
+         tab[i] = scanner.nextInt();
+       }
+
+ 
+    int rob;
+
+    for(int a=0; a<9; a++)
+      {
+      for(int i=0; i<9; i++)
+        {
+          if(tab[i]>tab[i+1])
+          {
+            rob=tab[i];            
+            tab[i]=tab[i+1];
+            tab[i+1]=rob;
+          }
+        }
+      }
+      
+     System.out.println("Posortowane elementy wprowadzone do tablicy: "); 
+      for(int i=0; i<10; i++)
+        {
+          System.out.print(tab[i] + " "); 
+        }
   }
 }
+  
